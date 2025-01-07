66d9274eec0e220b43fc5ae72eb275e7a95814d5
mich11y <yeoleemin@gmail.com>
2025-01-07 03:12:03 +0000

    Third Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var age = 20;
+age = 65;
+age = age+1;
+console.log(age);
+
+
+var username = "michelle";
+var password = "techup";
+
+var isFemale = false;
+var isLoggedIn = false;
+var blog_posts = ["Hi, Welcome to TechUp", "How are you?"]
+
+
+console.log(username);
+console.log(password);
+console.log(isFemale);
+console.log(isLoggedIn);
+console.log(blog_posts);
+
+if (isFemale == true) {
+    console.log("You are female");
+}
+
+if (isFemale == true) {
+    console.log("You are female")
+} else {
+    console.log("You are not female")
+}
+
+while (age < 70) {
+    console.log("Your age is " + age);
+    age = age + 1;
+}
+
+function sayHello() {
+    alert("Hello everyone");
+}
